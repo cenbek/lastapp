1bd0d6041c7cacd39a8a8cb29b588f231e3b560b
cenbek <sa.ordekci@gmail.com>
2013-01-25 16:28:30 +0200

    stpe_definitons klasoru eklendi

--- test/unit/helpers/home_helper_test.rb
+++ test/unit/helpers/home_helper_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
+require 'minitest/autorun'
 
-class HomeHelperTest < ActionView::TestCase
+class HomeHelperTest < MiniTest::Unit::TestCase
+  def setup
+    @register = CashRegister.new
+  end
+  def test_default_is_zero
+    assert_equal 0, @register.total
+  end
 end
+
