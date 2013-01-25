e0d826afe82b33ffb766b87d04bee98fee29b9ad
cenbek <sa.ordekci@gmail.com>
2013-01-25 15:49:24 +0200

    proje yaratildi ve ilk dosyalar oluşturuldu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lastapp::Application.config.secret_token = '2088c656b54600d0d647582eb184d9d92d61fd48e238b066cfcfeda043ccd0c532fc61001146fcd13410120678849628a94300edbb1bde3d26dda296fe0df193'
